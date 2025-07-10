b4f87a770a57a7c2b33f95f0dd212874fb8daa65
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-07-10 23:30:22 +0100

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
