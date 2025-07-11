9849193084cbcc14f70537236790be9c416521fd
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-07-11 17:31:25 +0100

    Initial commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <>
+      <section id="hero" className="noisy">
+        <h1 className="title">MOJITO</h1>
+        <img
+          src="/images/hero-left-leaf.png"
+          alt="left-leaf"
+          className="left-leaf"
+        />
+        <img
+          src="/images/hero-right-leaf.png"
+          alt="right-leaf"
+          className="right-leaf"
+        />
 
-export default Hero
+        <div className="body">
+          {/* <img src="/images/arrow.png" alt="arrow" className="arrow" /> */}
+
+          <div className="content">
+            <div className="space-y-5 hidden md:block">
+              <p>Cool. Crisp. Classic.</p>
+              <p className="subtitle">
+                Sip the Spirit <br /> of Summer
+              </p>
+            </div>
+
+            <div className="view-cocktails">
+              <p className="subtitle">
+                Every cocktail on our menu is a blend of premium ingredients,
+                creative flair, and timeless recipes — designed to delight your
+                senses.
+              </p>
+              <a href="#cocktails">View cocktails</a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
