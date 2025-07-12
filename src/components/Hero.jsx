0451d61bf07582f73052a4e8aede068cb209f500
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-07-12 09:52:45 +0100

    Initial commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -107,6 +107,16 @@ const Hero = () => {
           </div>
         </div>
       </section>
+
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          muted
+          playsInline
+          preload="auto"
+          src="/videos/output.mp4"
+        />
+      </div>
     </>
   );
 };
