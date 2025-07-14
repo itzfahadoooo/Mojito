bc06a7d69647d76a0afd73068dfaf04f82ff6b45
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-07-14 21:30:03 +0100

    Initial commit

--- src/components/Cocktails.jsx
+++ src/components/Cocktails.jsx
@@ -26,7 +26,6 @@ const Cocktails = () => {
 	<section id="cocktails" className="noisy">
 	 <img src="/images/cocktail-left-leaf.png" alt="l-leaf" id="c-left-leaf" />
 	 <img src="/images/cocktail-right-leaf.png" alt="r-leaf" id="c-right-leaf" />
-	 
 	 <div className="list">
 		<div className="popular">
 		 <h2>Most popular cocktails:</h2>
