b343ed6868bbec237e12a997442ccc89fc2f1adf
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-07-22 23:45:31 +0100

    Initial commit

--- src/components/Cocktails.jsx
+++ src/components/Cocktails.jsx
@@ -8,11 +8,12 @@ const Cocktails = () => {
       scrollTrigger: {
 
         trigger: "#cocktails",
+        
 
         start: "top 30%",
 
         end: "bottom 80%",
-        
+
         scrub: true,    
         
       },
